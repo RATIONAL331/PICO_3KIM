0886f93372e3f01bbf51f48bb3fb6e06083f26aa
RATIONAL331 <rational331@gmail.com>
2020-04-03 16:52:19 +0900

    env제거

--- project_venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\rational331\documents\github\pico_3kim\project_venv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
