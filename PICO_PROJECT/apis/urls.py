759932fb4d171a604ccc0bebe4b110f7445182ad
RATIONAL331 <rational331@gmail.com>
2020-04-03 21:10:59 +0900

    다시시작합니다.ㅇ

--- PICO_PROJECT/apis/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserCreateView, UserLoginView, UserLogoutView
-
-urlpatterns = [
-    path('v1/user/create/', UserCreateView.as_view(), name='apis_v1_user_create'),
-    path('v1/user/login/', UserLoginView.as_view(), name='apis_v1_user_login'),
-    path('v1/user/logout/', UserLogoutView.as_view(), name='apis_v1_user_logout'),
-]
