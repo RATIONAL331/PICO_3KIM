ba6455b7ce3ffa6cd6910ba78bc3d2672197771d
RATIONAL331 <rational331@gmail.com>
2020-04-13 15:21:35 +0900

    chap 10 finish

--- PICO_PROJECT/PICO_PROJECT/urls.py
+++ PICO_PROJECT/PICO_PROJECT/urls.py
@@ -20,8 +20,14 @@ from django.conf import settings
 
 from .views import HomeView
 
+from .views import UserCreateView, UserCreateDoneTV
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', UserCreateView.as_view(), name='register'),
+    path('accounts/register/done', UserCreateDoneTV.as_view(), name='register_done'),
 
     path('', HomeView.as_view(), name='home'),
     path('bookmark/', include('bookmark.urls')),
