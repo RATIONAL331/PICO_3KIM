d133ff2673240b3cdfa42f555145bf3af88df8eb
RATIONAL331 <rational331@gmail.com>
2020-04-13 22:48:34 +0900

    chap11 finish

--- PICO_PROJECT/blog/views.py
+++ PICO_PROJECT/blog/views.py
@@ -9,6 +9,11 @@ from django.conf import settings
 from django.db.models import Q
 from django.shortcuts import render
 
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from PICO_PROJECT.views import OwnerOnlyMixin
+
 # Create your views here.
 class PostLV(ListView):
     model = Post
@@ -47,6 +52,31 @@ class PostDAV(DayArchiveView):
 class PostTAV(TodayArchiveView):
     model = Post
     date_field = 'modify_dt'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'slug', 'description', 'content', 'tags']
+    initial = {'slug':'auto-filling-do-not-input'}
+    success_url = reverse_lazy('blog:index')
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+class PostChangeLV(LoginRequiredMixin, ListView):
+    template_name = 'blog/post_change_list.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(owner=self.request.user)
+
+class PostUpdateView(OwnerOnlyMixin, UpdateView):
+    model = Post
+    fields = ['title', 'slug', 'description', 'content', 'tags']
+    success_url = reverse_lazy('blog:index')
+
+class PostDeleteView(OwnerOnlyMixin, DeleteView):
+    model = Post
+    success_url = reverse_lazy('blog:index')
 
 class TagCloudTV(TemplateView):
     template_name = 'taggit/taggit_cloud.html'
