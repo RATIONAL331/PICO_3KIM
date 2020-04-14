2e8116e79878c6b733f3b42b0e762c0c88d90d30
RATIONAL331 <rational331@gmail.com>
2020-04-15 01:16:14 +0900

    chap12 finish

--- PICO_PROJECT/photo/models.py
+++ PICO_PROJECT/photo/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Album(models.Model):
     name = models.CharField(max_length=30)
     description = models.CharField('One Line Description', max_length=100, blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='OWNER', blank=True, null=True)
+
     class Meta:
         ordering = ('name',)
     
@@ -21,6 +24,7 @@ class Photo(models.Model):
     # image = ThumbnailImageField(upload_to='photo/%Y/%m')
     image = models.ImageField('IMAGE', upload_to='SorlPhoto/%Y')
     upload_dt = models.DateTimeField('Upload Date', auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='OWNER', blank=True, null=True)
 
     class Meta:
         ordering = ('title', )
