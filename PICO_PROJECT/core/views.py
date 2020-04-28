98d8d799277b55a59ab2e3caf505d80a3ec38a80
RATIONAL331 <rational331@gmail.com>
2020-04-28 14:47:57 +0900

    view update

--- PICO_PROJECT/core/views.py
+++ PICO_PROJECT/core/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from django.contrib.auth.models import User
 from django.shortcuts import Http404
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from .models import Profile
+from .models import Profile, ProfilePicoInfoLog
 from photo.models import Photo
+from .forms import ChargeForm
 
 class ProfileView(TemplateView):
     template_name = 'profile.html'
@@ -25,3 +26,18 @@ class ChargeView(LoginRequiredMixin, TemplateView):
     template_name = 'charge.html'
     model = User
     
+    def get(self, request, *args, **kwargs):
+        form = ChargeForm()
+        return render(request, 'charge.html', {'form':form, 'user':request.user})
+
+    def post(self, request, *args, **kwargs):
+        form = ChargeForm(request.POST)
+        if form.is_valid():
+            charging = ProfilePicoInfoLog()
+            charging.profile = request.user.profile
+            charging.donator = request.user
+            charging.PICOIN = form.cleaned_data['PICOIN']
+            charging.where = None
+            charging.save()
+            request.user.profile.PICOIN += form.cleaned_data['PICOIN']
+            return redirect('profile', request.user.username)
